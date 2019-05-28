5cf5296c7219e5355ca56431d4ccd1744394b661
Corina Wong <hello@corinawong.com>
2019-05-28 11:28:54 -0400

    Put nav bar in pages that need it

--- src/views/Resources.js
+++ src/views/Resources.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import searchIcon from '../images/musica-searcher.svg';
 import map from '../images/MyHealthMyWay - Map Kingston.svg';
 import pin from '../images/MyHealthMyWay - Map Kingston - green flag only.png';
+import NavBar from '../components/NavBar';
 
 const Intro = styled.p`
   margin: 1em;
@@ -42,41 +43,44 @@ const List = styled.div`
 
 function Resources() {
   return (
-    <div className="page">
-      <h1>Resources</h1>
-      <Intro>
-        Telehealth for medical advice- Toll-free:
-        <strong className="primary">1-866-797-0000</strong>
-      </Intro>
+    <div>
+      <NavBar />
+      <div className="content">
+        <h1>Resources</h1>
+        <Intro>
+          Telehealth for medical advice- Toll-free:
+          <strong className="primary">1-866-797-0000</strong>
+        </Intro>
 
-      <Intro>
-        Ask if your workplace has an Employment Assistance Programs (EAP)
-      </Intro>
-      <Search className="rounded-container">
-        <img src={searchIcon} alt="search icon" />
-        K7L3P5
-      </Search>
-      <Map className="rounded-container">
-        <img src={map} alt="map" />
-      </Map>
+        <Intro>
+          Ask if your workplace has an Employment Assistance Programs (EAP)
+        </Intro>
+        <Search className="rounded-container">
+          <img src={searchIcon} alt="search icon" />
+          K7L3P5
+        </Search>
+        <Map className="rounded-container">
+          <img src={map} alt="map" />
+        </Map>
 
-      <h2>Group Support</h2>
-      <List className="rounded-container">
-        <img src={pin} alt="pin" />
-        <div>
-          <p>Depression & Aniexty Support</p>
-          <p> Every Wednesday 6:30pm</p>
-          <p> Bethel Church - 314 Johnson St</p>{' '}
-        </div>
-      </List>
+        <h2>Group Support</h2>
+        <List className="rounded-container">
+          <img src={pin} alt="pin" />
+          <div>
+            <p>Depression & Aniexty Support</p>
+            <p> Every Wednesday 6:30pm</p>
+            <p> Bethel Church - 314 Johnson St</p>{' '}
+          </div>
+        </List>
 
-      <List className="rounded-container">
-        <img src={pin} alt="pin" />
-        <div>
-          <p>Boundary Mangement</p>
-          <p> John Deutsch University Centre</p>
-        </div>
-      </List>
+        <List className="rounded-container">
+          <img src={pin} alt="pin" />
+          <div>
+            <p>Boundary Mangement</p>
+            <p> John Deutsch University Centre</p>
+          </div>
+        </List>
+      </div>
     </div>
   );
 }
--- src/views/Talk.js
+++ src/views/Talk.js
@@ -5,6 +5,7 @@ import call from '../images/undraw_calling_kpbp.svg';
 import chat from '../images/undraw_online_world_mc1t.svg';
 import video from '../images/undraw_video_call_kxyp.svg';
 import SquareButton from '../components/SquareButton';
+import NavBar from '../components/NavBar';
 
 const Option = styled.section`
   margin: 1em 0;
@@ -26,49 +27,53 @@ const Option = styled.section`
 
 function Talk({ match }) {
   return (
-    <div className="page">
-      <Option>
-        <div className="rounded-container">
-          <h1>Call</h1>
-          <img src={call} />
-        </div>
-        <div>
-          <p>Details:</p>
-          <p>Provincial Mental Crisis Number</p>
-          <p>1-888-234-5678</p>
-          <p>Open 24/7</p>
-          <SquareButton text="Start" />
-        </div>
-      </Option>
-      <Option>
-        <div className="rounded-container">
-          <h1>Chat</h1>
-          <img src={chat} />
-        </div>
-        <div>
-          <p>Details:</p>
-          <p>Provincial Mental Crisis Number</p>
-          <p>1-888-234-5678</p>
-          <p>Open 24/7</p>
+    <div>
+      <NavBar />
 
-          <Link to="/chat">
+      <div className="content">
+        <Option>
+          <div className="rounded-container">
+            <h1>Call</h1>
+            <img src={call} />
+          </div>
+          <div>
+            <p>Details:</p>
+            <p>Provincial Mental Crisis Number</p>
+            <p>1-888-234-5678</p>
+            <p>Open 24/7</p>
             <SquareButton text="Start" />
-          </Link>
-        </div>
-      </Option>
-      <Option>
-        <div className="rounded-container">
-          <h1>Video</h1>
-          <img src={video} />
-        </div>
-        <div>
-          <p>Details:</p>
-          <p>Provincial Mental Crisis Number</p>
-          <p>1-888-234-5678</p>
-          <p>Open 24/7</p>
-          <SquareButton text="See Options" />
-        </div>
-      </Option>
+          </div>
+        </Option>
+        <Option>
+          <div className="rounded-container">
+            <h1>Chat</h1>
+            <img src={chat} />
+          </div>
+          <div>
+            <p>Details:</p>
+            <p>Provincial Mental Crisis Number</p>
+            <p>1-888-234-5678</p>
+            <p>Open 24/7</p>
+
+            <Link to="/chat">
+              <SquareButton text="Start" />
+            </Link>
+          </div>
+        </Option>
+        <Option>
+          <div className="rounded-container">
+            <h1>Video</h1>
+            <img src={video} />
+          </div>
+          <div>
+            <p>Details:</p>
+            <p>Provincial Mental Crisis Number</p>
+            <p>1-888-234-5678</p>
+            <p>Open 24/7</p>
+            <SquareButton text="See Options" />
+          </div>
+        </Option>
+      </div>
     </div>
   );
 }
